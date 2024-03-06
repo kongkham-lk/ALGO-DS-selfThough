f2605f6efa94838a24057da8f3f1eb82da7522f8
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-03-05 22:28:56 -0800

    Solve find the min/max size of island graph problem

---
+++ DFS - BFS/Find Island Size/Find Island Size/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string[][] grid =
+        {
+            new string[] {"W", "L", "W", "W", "L"},
+            new string[] {"W", "L", "W", "W", "L"},
+            new string[] {"W", "L", "W", "L", "W"},
+            new string[] {"W", "W", "L", "L", "W"},
+            new string[] {"L", "W", "W", "L", "L"},
+            new string[] {"L", "L", "W", "L", "W"},
+        };
+
+        int[] size = FindIslandSize(grid);
+
+        Console.WriteLine("MIN. island size is: " + size[0]); // return 2
+        Console.WriteLine("MAX. island size is: " + size[1]); // return 6
+    }
+
+    private static int[] FindIslandSize(string[][] grid)
+    {
+        int minSize = int.MaxValue;
+        int maxSize = int.MinValue;
+        List<string> visited = new();
+
+        for (int row = 0; row < grid.Length; row++)
+        {
+            for (int col = 0; col < grid[row].Length; col++)
+            {
+                if (!visited.Contains(row + "," + col) && grid[row][col].ToUpper().Equals("L"))
+                {
+                    int newSize = FoundIsland(grid, row, col, visited);
+
+                    minSize = Math.Min(minSize, newSize);
+                    maxSize = Math.Max(maxSize, newSize);
+                }
+            }
+        }
+        return new int[] {minSize, maxSize};
+    }
+
+    private static int FoundIsland(string[][] grid, int row, int col, List<string> visited)
+    {
+        if (visited.Contains(row + "," + col) || !grid[row][col].ToUpper().Equals("L"))
+            return 0;
+
+        visited.Add(row + "," + col);
+        int totalSize = 1;
+
+        while (col > 0)
+        {
+            int size = FoundIsland(grid, row, col - 1, visited);
+            if (size == 0)
+                break;
+            totalSize += size;
+        }
+        while (col < grid[row].Length - 1)
+        {
+            int size = FoundIsland(grid, row, col + 1, visited);
+            if (size == 0)
+                break;
+            totalSize += size;
+        }
+        while (row < grid.Length - 1 && col < grid[row].Length)
+        {
+            int size = FoundIsland(grid, row + 1, col, visited);
+            if (size == 0)
+                break;
+            totalSize += size;
+        }
+
+        return totalSize;
+    }
+}
