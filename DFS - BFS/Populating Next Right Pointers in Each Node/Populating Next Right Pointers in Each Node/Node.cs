5f03748f4c3f75afe56827ce375ddda68ae61391
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-03-21 21:54:19 -0700

    Solve Populating Next Right Pointers in Each Node

---
+++ DFS - BFS/Populating Next Right Pointers in Each Node/Populating Next Right Pointers in Each Node/Node.cs
@@ -0,0 +1,22 @@
+﻿public class Node
+{
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() { }
+
+    public Node(int _val)
+    {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left = null, Node _right = null, Node _next = null)
+    {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
---
+++ DFS - BFS/Populating Next Right Pointers in Each Node/Populating Next Right Pointers in Each Node/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Xml.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Node root = new Node(1,
+            new Node(2, new Node(4), new Node(5)),
+            new Node(3, new Node(6), new Node(7)));
+
+        IterateNodes(Connect(root));
+    }
+
+    public static Node Connect(Node root)
+    {
+        if (root == null) return root;
+
+        Queue<Node> queue = new Queue<Node>();
+        queue.Enqueue(root);
+
+        while (queue.Any())
+        {
+            int size = queue.Count;
+            Node prevNode = null;
+
+            while (size > 0)
+            {
+                Node currNode = queue.Dequeue();
+
+                if (prevNode != null)
+                    prevNode.next = currNode;
+
+                prevNode = currNode;
+
+                if (currNode.left != null)
+                    queue.Enqueue(currNode.left);
+                if (currNode.right != null)
+                    queue.Enqueue(currNode.right);
+
+                size--;
+            }
+
+            prevNode.next = null;
+        }
+        return root;
+    }
+
+    public static void IterateNodes(Node root)
+    {
+        Console.Write("[ ");
+
+        Queue<Node> queue = new Queue<Node>();
+        queue.Enqueue(root);
+
+        while (queue.Any())
+        {
+            int size = queue.Count;
+
+            while (size > 0)
+            {
+                Node currNode = queue.Dequeue();
+                Console.Write(currNode.val + " ");
+
+                if (currNode.next == null)
+                    Console.Write("# ");
+
+                if (currNode.left != null)
+                    queue.Enqueue(currNode.left);
+                if (currNode.right != null)
+                    queue.Enqueue(currNode.right);
+
+                size--;
+            }
+        }
+        Console.WriteLine("]\n");
+    }
+}
